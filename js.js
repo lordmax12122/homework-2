c9d60527ad703f41918b0b9222ea5b145ee10326
lordmax12122 <116733872+lordmax12122@users.noreply.github.com>
2024-10-08 20:55:28 +0300

    hw2

---
+++ js.js
@@ -0,0 +1,40 @@
+const celsium = 21;
+const farenheit = (celsium * 5) + 32;
+console.log(farenheit + "°F");
+
+const monthDays = 31;
+const dayHours = monthDays * 60;
+const hourMinutes = dayHours * 60;
+console.log(hourMinutes + "У місяці" + hourMinutes + "хвилин");
+
+const health = 100;
+const energy = 100;
+health -= 100;
+console.log("Ви мертвий, у вас" + health + "життя" );
+
+const expenses = 2220;
+const discount = 0.9;
+const expensesWithDiscount = expenses * discount;
+console.log("Заплатіть " + expensesWithDiscount + "гривень");
+
+const number = 6.66;
+const roundedNumber = Math.floor(number);
+console.log("Заокруглене число дорівнює:" + roundedNumber);
+
+const floatNumber = "6.66";
+const floatResult = parseFloat(floatNumber);
+console.log("Ваш результат:" + floatResult);
+
+const intNumber = "666";
+const intResult = parseInt(intNumber);
+console.log("Ваш результат:" + intResult);
+
+const mathNumber = "9";
+const mathResult = Math.sqrt(mathNumber);
+console.log("Ваш результат:" + mathResult);
+
+const intNumber2 = "666";
+const intResult2 = int.sqrt(intNumber2);
+console.log("Ваш результат:" + intResult2);
+const stringResult = int.sqrt(intResult2);
+console.log("Ваш результат:" + stringResu
